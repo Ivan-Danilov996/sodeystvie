e7639bd7dd43b9eddeb5df83142fb1d778eacf20
Ivan Danilov <ivandanilov996@yandex.ru>
2020-09-17 15:39:28 +0300

    ihone x

--- js/script.js
+++ js/script.js
@@ -7,10 +7,34 @@ $(document).ready(function() {
         centerMode: true,
         centerPadding: '0px',
         variableWidth: true,
+        speed: 1100,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 425,
+              settings: {
+                slidesToShow: 1,
+                dots: true,
+              }
+            },
+        ],
     })
 
     $('.accordeon-item__trigger').click(function() {
         $(this).find('.plus').toggleClass("plus-active");
         $(this).next('.accordeon-item__content').slideToggle();
     })
+
+    $("#phone").mask("+7(999) 999-9999");
+
+    $("#modal-phone").mask("+7(999) 999-9999");
+
+
+
+    $(".cause__btn").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".price").offset().top +'px'
+        }, {
+            duration: 2000});
+    });
 })
